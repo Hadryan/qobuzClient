385388ad17566ed3f6b5469cc492c356d7a9cffd
James Corbett <james.corbett@rma-consulting.com>
2014-06-28 00:46:08 +0100

    Dynamic ports for websocket client and beautify

--- client/js/component/service/auroraPlayer.js
+++ client/js/component/service/auroraPlayer.js
@@ -1,70 +1,72 @@
-define(function (require) {
+define(function(require) {
 
-  'use strict';
+    'use strict';
 
-  /**
-   * Module dependencies
-   */
+    /**
+     * Module dependencies
+     */
 
-  var defineComponent = require('flight/lib/component');
+    var defineComponent = require('flight/lib/component');
 
-  /**
-   * Module exports
-   */
+    /**
+     * Module exports
+     */
 
-  return defineComponent(auroraPlayer);
+    return defineComponent(auroraPlayer);
 
-  /**
-   * Module function
-   */
+    /**
+     * Module function
+     */
 
-  function auroraPlayer() {
-    var player;
+    function auroraPlayer() {
+        var player;
 
-//      var _sampleRate = (function() {
-//          var AudioContext = (window.AudioContext || window.webkitAudioContext);
-//          if (!AudioContext)
-//              return 44100;
-//
-//          return new AudioContext().sampleRate;
-//      }());
+        //      var _sampleRate = (function() {
+        //          var AudioContext = (window.AudioContext || window.webkitAudioContext);
+        //          if (!AudioContext)
+        //              return 44100;
+        //
+        //          return new AudioContext().sampleRate;
+        //      }());
 
-    this.defaultAttrs({
+        this.defaultAttrs({
 
-    });
-
-    this.playTrack = function(ev, data){
-        var url = data.url;
-        if (player) player.stop();
-//        player = AV.Player.fromURL(url);
-        player = AV.Player.fromWebSocket('ws://localhost:8080', url)
-        player.play();
-
-        player.on('error', function(e) { throw e; });
-
-        player.on('buffer', function(percent) {
-            $('.pure-menu-heading')[0].innerHTML = percent
-//            console.log("bufferProgress " +  percent);
-//            window.alert('percent')
         });
 
-        player.on('progress',   function(time) {
-            console.log("seekTime " +  time);
+        this.playTrack = function(ev, data) {
+            var url = data.url;
+            if (player) player.stop();
+            //        player = AV.Player.fromURL(url);
+            player = AV.Player.fromWebSocket('ws://' + location.hostname + ':' + location.port, url)
+            player.play();
+
+            player.on('error', function(e) {
+                throw e;
+            });
+
+            player.on('buffer', function(percent) {
+                $('.pure-menu-heading')[0].innerHTML = percent
+                //            console.log("bufferProgress " +  percent);
+                //            window.alert('percent')
+            });
+
+            player.on('progress', function(time) {
+                console.log("seekTime " + time);
+            });
+
+            player.on('duration', function(duration) {
+                console.log("duration " + duration);
+            });
+
+            player.on('metadata', function(data) {
+                console.log(data);
+            });
+        };
+
+        this.after('initialize', function() {
+            this.on(document, 'playTrackFromUrl', this.playTrack);
         });
 
-        player.on('duration', function(duration) {
-            console.log("duration " +  duration);
-        });
-
-        player.on('metadata', function(data) {
-            console.log(data);
-        });
-    };
-
-    this.after('initialize', function () {
-        this.on(document, 'playTrackFromUrl', this.playTrack);
-    });
-
-  }
+    }
 
 });
